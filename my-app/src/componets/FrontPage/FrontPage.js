3ee7c1e1b86698575931874db946bb1ba672616c
KevinKohutek04 <kohutekkevin7@gmail.com>
2024-06-22 15:15:17 -0500

    fix template

--- my-app/src/componets/FrontPage/FrontPage.js
+++ my-app/src/componets/FrontPage/FrontPage.js
@@ -1,28 +1,11 @@
 import React from "react";
 import './FrontPage.css';
+import sea from '../Assets/sea.png';
 
 function FrontPage() {
     return (
         <div className="MainBox">
-            <header className="Header">
-                <h1 className="Title">Our Fish</h1>
-                <h2 className="SubTitle">Explore our variety of fish</h2>
-            </header>
-            <main className="MainContent">
-                <section className="FishSection">
-                    <div className="FishCard">
-                        <img src="path/to/fish1.jpg" alt="Fish 1" className="FishImage"/>
-                        <h3 className="FishTitle">Fish 1</h3>
-                        <p className="FishDescription">Description of Fish 1.</p>
-                    </div>
-                    <div className="FishCard">
-                        <img src="path/to/fish2.jpg" alt="Fish 2" className="FishImage"/>
-                        <h3 className="FishTitle">Fish 2</h3>
-                        <p className="FishDescription">Description of Fish 2.</p>
-                    </div>
-                    {/* Add more fish cards as needed */}
-                </section>
-            </main>
+             <img src={sea} alt="Sea" />
         </div>
     );
 }
