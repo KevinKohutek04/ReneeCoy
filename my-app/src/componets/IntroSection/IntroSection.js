bfd757a7ba6efd12f22925fb622fe123544b9747
KevinKohutek04 <kohutekkevin7@gmail.com>
2024-06-23 17:26:12 -0500

    changes

--- my-app/src/componets/IntroSection/IntroSection.js
+++ my-app/src/componets/IntroSection/IntroSection.js
@@ -1,11 +1,18 @@
 import React from "react";
 import './IntroSection.css';
+import img from '../Assets/logo.png';
 
 function IntroSection () {
     return (
         <div className="IntroMain">
                 <div className="Top-Section">
-                    skibidi
+                    <div className="Top-Section-Nav">
+                        <a href="#">Home</a>
+                        <a href="#">About</a>
+                        <img className="Top-Section-Photo" src={img} alt="Logo" />
+                        <a href="#">Services</a>
+                        <a href="#">Contact</a>
+                     </div>
                 </div>
         </div>
     );
